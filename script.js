64f7ba912c39428fdf7a839ad0e9c6a633a75978
Drake Olejniczak <drake.olejniczak@gmail.com>
2024-03-26 13:19:24 -0400

    Bones of d3 network visualizer added to the project

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
     // Width and height
     var width = 800, height = 600;
 
-    // Setup the SVG
+    // Setup the SVG and the group (g) element
     var svg = d3.select("#network").append("svg")
         .attr("width", width)
-        .attr("height", height);
+        .attr("height", height)
+        .call(d3.zoom().on("zoom", function (event) {
+            g.attr("transform", event.transform);
+        }))
+        .append("g");
+
+    var g = svg.append("g"); // All visual elements will be added to this group
 
     // Sample data: nodes and links
     var nodes = [
@@ -18,28 +24,50 @@
         {source: 0, target: 4}
     ];
 
+    var drag = d3.drag()
+        .on("start", dragstarted)
+        .on("drag", dragged)
+        .on("end", dragended);
+
+    function dragstarted(event, d) {
+        if (!event.active) simulation.alphaTarget(0.3).restart(); // Reheat the simulation
+        d.fx = d.x; // Fix the position of the node
+        d.fy = d.y;
+    }
+
+    function dragged(event, d) {
+        d.fx = event.x; // Set the fixed position to the current pointer position
+        d.fy = event.y;
+    }
+
+    function dragended(event, d) {
+        if (!event.active) simulation.alphaTarget(0); // Cool down the simulation
+        d.fx = null; // Unfix the position
+        d.fy = null;
+    }
+
     // Setup the force layout
     var simulation = d3.forceSimulation(nodes)
-        .force("link", d3.forceLink(links).id(function(d) { return d.index; }))
-        .force("charge", d3.forceManyBody())
+        .force("link", d3.forceLink(links).id(d => d.index))
+        .force("charge", d3.forceManyBody().strength(-400))
         .force("center", d3.forceCenter(width / 2, height / 2));
 
-    // Create the link lines.
-    var link = svg.append("g")
+    // Create the link lines in the group
+    var link = g.append("g")
         .attr("class", "links")
         .selectAll("line")
         .data(links)
         .enter().append("line")
         .attr("stroke-width", 2);
 
-    // Create the node circles.
-    var node = svg.append("g")
+    // Create the node circles in the group
+    var node = g.append("g")
         .attr("class", "nodes")
         .selectAll("circle")
         .data(nodes)
         .enter().append("circle")
         .attr("r", 10)
-        .attr("fill", "red");
+        .call(drag);
 
     // Update positions each tick
     simulation.on("tick", function() {
